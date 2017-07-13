ace28892d9da1bbb1605b18bc1fd4c16418b8095
Thomas Welsch <welscht@osti.gov>
2017-07-12 23:02:02 -0400

    Beginning biblio work

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,7 @@ import DOECodeWizard from './components/DOECodeWizard';
 import Confirmation from './confirmation/Confirmation';
 import WorkflowManagement from './user/WorkflowManagement';
 import Splash from './splash/Splash';
+import BiblioPage from './dissemination/BiblioPage';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory,IndexRoute} from 'react-router';
@@ -19,6 +20,7 @@ class DOECodeRouter extends React.Component {
 			<Route path="/wizard" component={DOECodeWizard}/>
 			<Route path="/confirm" component={Confirmation}/>
 			<Route path="/projects" component={WorkflowManagement}/>
+			<Route path="/biblio" component={BiblioPage}/>
 		</Router>
 		);
 	}
